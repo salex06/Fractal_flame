e45aae9085a93459b7104f355a5fb4134825a90e
Alexey Sazonov <salex.9206@mail.ru>
2024-12-02 20:56:40 +0300

    refactor: change the type of the configuration variable
    Replace the configuration object as a string with an object of the ImageGeneratingConfiguration class

--- src/main/java/backend/academy/app/impl/FractalApplication.java
+++ src/main/java/backend/academy/app/impl/FractalApplication.java
@@ -4,6 +4,7 @@ import backend.academy.app.Application;
 import backend.academy.cli.CliParams;
 import backend.academy.format.ImageFormat;
 import backend.academy.image.FractalImage;
+import backend.academy.image.ImageGenerationConfig;
 import backend.academy.render.Renderer;
 import backend.academy.transformations.variations.Variation;
 import backend.academy.utils.IOHandler;
@@ -44,55 +45,46 @@ public class FractalApplication implements Application {
         int threadNumber = cliParams.threadsNumber();
 
         if (cliParams.useOneThread()) {
-            String config =
-                getConfig(width, height, affinesNumber, variations, samples, (short) itersPerSample, symmetry,
-                    1, format);
-            processGeneration(affinesNumber, new FractalImage(height, width), variations, samples,
-                (short) itersPerSample,
-                symmetry, 1, config, format);
+            ImageGenerationConfig config =
+                new ImageGenerationConfig(width, height, affinesNumber, variations, samples, (short) itersPerSample,
+                    symmetry,
+                    format, 1);
+            processGeneration(new FractalImage(height, width), 1, config);
         }
         if (cliParams.threadsNumber() > 1) {
-            String config =
-                getConfig(width, height, affinesNumber, variations, samples, (short) itersPerSample, symmetry,
-                    threadNumber, format);
-            processGeneration(affinesNumber, new FractalImage(height, width), variations, samples,
-                (short) itersPerSample,
-                symmetry, threadNumber, config, format);
+            ImageGenerationConfig config =
+                new ImageGenerationConfig(width, height, affinesNumber, variations, samples, (short) itersPerSample,
+                    symmetry,
+                    format, threadNumber);
+            processGeneration(new FractalImage(height, width), threadNumber, config);
         }
-
     }
 
     @SuppressWarnings("ParameterNumber")
     private void processGeneration(
-        int affinesNumber,
         FractalImage fractalImage,
-        List<Variation> variations,
-        int samples,
-        short itersPerSample,
-        int symmetry,
         int threadNumber,
-        String config,
-        ImageFormat format
+        ImageGenerationConfig config
     ) throws IOException {
-        ioHandler.write("Генерация изображения начата... [" + threadNumber + " потоков]\n");
+        ioHandler.write("Генерация изображения начата... [Количество потоков: " + threadNumber + "]\n");
         long start = System.nanoTime();
         FractalImage image = renderer.render(
-            affinesNumber,
+            config.affinesCount(),
             new FractalImage(fractalImage.height(), fractalImage.width()),
-            variations,
-            samples,
-            itersPerSample,
-            symmetry,
+            config.variationList(),
+            config.samples(),
+            config.iterPerSample(),
+            config.symmetry(),
             threadNumber
         );
         long elapsedTime = System.nanoTime() - start;
 
-        saveData(image, config, format, elapsedTime);
+        saveData(image, config, elapsedTime);
         ioHandler.write("Генерация изображения завершена!\n");
     }
 
     @SuppressFBWarnings({"PATH_TRAVERSAL_IN", "PATH_TRAVERSAL_OUT"})
-    private void saveData(FractalImage image, String config, ImageFormat format, long elapsedTime) throws IOException {
+    private void saveData(FractalImage image, ImageGenerationConfig config, long elapsedTime) throws IOException {
         File dir =
             new File(PATH_TO_ROOT_DIR,
                 LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-LL-yyyy_HHmmss")));
@@ -102,39 +94,11 @@ public class FractalApplication implements Application {
         }
         String logPath = dir + "/info.log";
         IOHandler fileHandler = new IOHandlerImpl(System.in, Files.newOutputStream(Path.of(logPath)));
-        fileHandler.write(config);
+        fileHandler.write(config.getConfig());
         fileHandler.write("Затраченное время (в секундах): " + elapsedTime / NANOSECONDS_IN_SECOND + '\n');
 
-        String imagePath = dir + "/image." + format.name().toLowerCase();
-        ImageUtils.save(image, Path.of(imagePath), format);
-    }
-
-    @SuppressWarnings("ParameterNumber")
-    private String getConfig(
-        int width,
-        int height,
-        int affineCount,
-        List<Variation> variations,
-        int samples,
-        short iterPerSamples,
-        int symmetry,
-        int threadNumber,
-        ImageFormat imageFormat
-    ) {
-        StringBuilder config = new StringBuilder();
-        config.append("Ширина изображения: ").append(width).append('\n');
-        config.append("Высота изображения: ").append(height).append('\n');
-        config.append("Количество аффинных преобразований: ").append(affineCount).append('\n');
-        config.append("Выбранные вариации: \n");
-        for (Variation variation : variations) {
-            config.append('\t').append(variation.getClass().getSimpleName()).append('\n');
-        }
-        config.append("Количество сэмплов: ").append(samples).append('\n');
-        config.append("Количество итераций на сэмпл: ").append(iterPerSamples).append('\n');
-        config.append("Количество осей симметрии: ").append(symmetry).append('\n');
-        config.append("Количество потоков: ").append(threadNumber).append('\n');
-        config.append("Формат файла: ").append(imageFormat.name()).append('\n');
-        return config.toString();
+        String imagePath = dir + "/image." + config.imageFormat().name().toLowerCase();
+        ImageUtils.save(image, Path.of(imagePath), config.imageFormat());
     }
 
     private static final long NANOSECONDS_IN_SECOND = 1_000_000_000;
