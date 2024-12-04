4a54203aa30f477b43787102664242f5d4037d5e
Alexey Sazonov <salex.9206@mail.ru>
2024-12-04 10:35:03 +0300

    docs: add JavaDoc for the ImageProcessor interface

--- src/main/java/backend/academy/processing/ImageProcessor.java
+++ src/main/java/backend/academy/processing/ImageProcessor.java
@@ -2,7 +2,14 @@ package backend.academy.processing;
 
 import backend.academy.image.FractalImage;
 
+/**
+ * Provides process method which performs some image processing
+ */
 @FunctionalInterface
 public interface ImageProcessor {
+    /**
+     * Process the image
+     * @param image the image that needs to be processed
+     */
     void process(FractalImage image);
 }
