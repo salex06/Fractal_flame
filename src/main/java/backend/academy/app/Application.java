8ea71d4b94a43d10eb4953a64a2b830dcba298c6
Alexey Sazonov <salex.9206@mail.ru>
2024-12-05 07:32:56 +0300

    docs: add JavaDocs for the Application class

--- src/main/java/backend/academy/app/Application.java
+++ src/main/java/backend/academy/app/Application.java
@@ -2,6 +2,19 @@ package backend.academy.app;
 
 import java.io.IOException;
 
+/**
+ * A class that has only one run method
+ * and is required to run the application.
+ * This class represents the general logic
+ * of the program, interacts with its components
+ * to obtain a result
+ */
 public interface Application {
+    /**
+     * Starts the application
+     *
+     * @param args the command line parameters
+     * @throws IOException is any i/o errors occur
+     */
     void run(String[] args) throws IOException;
 }
